d1181f713551ca44846bb492391e4662214992c7
Chloe DeVries <chloe@reverb.com>
2020-05-04 19:32:29 -0500

    edits

--- main.py
+++ main.py
@@ -10,19 +10,27 @@ from pitchfork_predictor.create_pitchfork_lineups_data import *
 from pitchfork_predictor.cleaning_functions import *
 from pitchfork_predictor.other_utils import *
 
+def predict_by_top_probabilities(probability_preds, full_df):
+    percent_true = full_df.played_fest.mean()
+    number_true = pd.DataFrame(probability_preds)[1].shape[0] * percent_true
+    true_prediction_cutoff = pd.DataFrame(probability_preds)[1].sort_values().tail(int(number_true)).min()
+
+    reviews_preds_by_prob = (pd.DataFrame(probability_preds)[1] >= true_prediction_cutoff)
+    return reviews_preds_by_prob
 
 # import & clean datasets
 # lineups
-filepath = 'data/raw_p4k_lineups.txt'
+filepath = 'pitchfork_predictor/data/raw_p4k_lineups.txt'
 lineups = get_lineups_data(filepath)
 
 # reviews
-reviews_df = pd.read_csv('data/pitchfork_reviews.csv')
+reviews_df = pd.read_csv('pitchfork_predictor/data/pitchfork_reviews.csv')
 reviews = clean_reviews_df(reviews_df)
 
 # features
-articles_df = pd.read_csv('data/pitchfork_features_with_article_txt.csv')
+articles_df = pd.read_csv('pitchfork_predictor/data/pitchfork_features_with_article_txt.csv')
 articles = clean_features_df(articles_df)
+print('data uploaded')
 
 
 # first, train the reviews model
@@ -62,11 +70,12 @@ reviews_full_df['performance_x_score'] = reviews_full_df.performance_mention \
                                          * reviews_full_df.score_norm
 reviews_full_df['chicago_x_performance'] = reviews_full_df.chicago_based \
                                            * reviews_full_df.performance_mention
+print('reviews data munged')
 
 # tf-idf vectorization of reviews texts
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-tfidf_vectorizer = TfidfVectorizer(stop_words = 'english')
+tfidf_vectorizer = TfidfVectorizer(stop_words = 'english', min_df=100)
 
 X_reviews = tfidf_vectorizer.fit_transform(reviews_full_df['review']) \
                              .toarray()
@@ -74,60 +83,87 @@ X_reviews = tfidf_vectorizer.fit_transform(reviews_full_df['review']) \
 # X_reviews.tofile("saved_features/x_reviews.txt")
 # X_reviews = np.fromfile("saved_features/x_reviews.txt")
 
-reviews_frequency_matrix = pd.DataFrame(X_reviews, columns
-                                = tfidf_vectorizer.get_feature_names())
-reviews_frequency_matrix.to_csv("saved_features/reviews_frequency_matrix.csv")
+reviews_frequency_matrix = pd.DataFrame(X_reviews,
+                                        columns = tfidf_vectorizer.get_feature_names())
+reviews_frequency_matrix.to_csv("pitchfork_predictor/saved_features/reviews_frequency_matrix_mindf100.csv")
+print('tf-df done')
+print(reviews_frequency_matrix.shape)
+
+#reviews_frequency_matrix = pd.read_csv("pitchfork_predictor/saved_features/reviews_frequency_matrix_mindf100.csv")
+#print("loaded frequency matrix")
+#print(reviews_frequency_matrix.shape)
 
 # add new tf-idf features to df
 reviews_full_full_df = reviews_full_df.reset_index(drop=True) \
                             .join(reviews_frequency_matrix,
-                                  lsuffix='_review')
+                                  lsuffix='_og')
 
 # join in y var
 reviews_model_df = join_lineups(reviews_full_full_df, lineups, 'left')
 
 
 # train reviews sub-model
-reviews_training_set = reviews_model_df.loc[
-                    reviews_model_df.fest_date != '2019-07-15']
-reviews_test_set = reviews_model_df.loc[
-                    reviews_model_df.fest_date == '2019-07-15']
-
 reviews_inputs = ['score_norm', 'avg_cumul_score_norm',
                   'best', 'previous_reviews_count',
                   'performance_mention', 'chicago_based', 'reissue',
                   'days_to_announcement_norm',
                   'Rock', 'Rap', 'Jazz', 'Experimental', 'Pop/R&B',
                   'Electronic', 'Metal', 'Global', 'Folk/Country',
-                  'score_x_months', 'reissue_x_months',
-                  'cumul_score_x_months', 'chicago_x_months',
-                  'performance_x_months', 'best_x_months',
+                  'score_x_days', 'reissue_x_days',
+                  'cumul_score_x_days', 'chicago_x_days',
+                  'performance_x_days', 'best_x_days',
                   'chicago_x_score', 'performance_x_score',
                   'chicago_x_performance'] \
-                    + tfidf_vectorizer.get_feature_names()
+                    + reviews_frequency_matrix.columns.tolist()
+                    #+ tfidf_vectorizer.get_feature_names()
 
 output = ['played_fest']
 
-X_train = reviews_training_set[reviews_inputs]
-y_train = reviews_training_set[output].astype('int')
-X_test = reviews_test_set[reviews_inputs]
-y_test = reviews_test_set[output].astype('int')
+from sklearn.model_selection import StratifiedShuffleSplit
+
+X = reviews_model_df[reviews_inputs]
+y = reviews_model_df[output]
+
+sss = StratifiedShuffleSplit(n_splits=2, test_size=0.33, random_state=42)
+sss.split(X, y)
+
+for train_index, test_index in sss.split(X, y):
+    X_train, X_test = pd.DataFrame(np.asarray(X)[train_index], columns = reviews_inputs), \
+                            pd.DataFrame(np.asarray(X)[test_index], columns = reviews_inputs)
+    y_train, y_test = pd.DataFrame(np.asarray(y)[train_index], columns = output), \
+                            pd.DataFrame(np.asarray(y)[test_index], columns = output)
+
+print("created training and testing groups")
 
 reviews_model = RandomForestClassifier()
 reviews_model.fit(X_train, y_train)
+print("fit model")
 
 # look at basic performance
 reviews_training_preds = reviews_model.predict(X_train)
 reviews_training_prob_preds = reviews_model.predict_proba(X_train)
 
 print("training dataset:")
+print("from model:")
 print_performance_metrics(y_train,
                           reviews_training_preds)
-print("test dataset:")
+print("by probabilities:")
+reviews_training_preds_by_prob = predict_by_top_probabilities(reviews_training_prob_preds, reviews_model_df)
+print_performance_metrics(y_train, reviews_training_preds_by_prob)
+
 reviews_preds = reviews_model.predict(X_test)
 reviews_prob_preds = reviews_model.predict_proba(X_test)
 
+print("test dataset:")
+print("from model:")
 print_performance_metrics(y_test, reviews_preds)
+print("by probabilities:")
+reviews_preds_by_prob = predict_by_top_probabilities(reviews_prob_preds, reviews_model_df)
+print_performance_metrics(y_test, reviews_preds_by_prob)
+
+
+import sys
+sys.exit('break')
 
 
 # next, the articles model
